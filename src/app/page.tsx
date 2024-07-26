848a5e11871f6398679528dfbe41102440d8489d
lppduy <lephamphuongduy@gmail.com>
2024-07-26 13:27:15 +0700

    feat: Add Clerk authentication to sign-in and sign-up pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,21 @@
-import { Button, Input } from "antd";
-
-export default function Home() {
+import { UserButton } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs/server";
+import { Button } from "antd";
+export default async function Home() {
+  const user = await currentUser();
+  const name = user?.firstName +' '+ user?.lastName;
+  const email = user?.emailAddresses[0].emailAddress;
+  const clerkUserId = user?.id;
+  console.log(user);
   return (
     <div className="p-5 flex flex-col gap-5 items-start">
       <h1 className="text-3xl">
         ZiCV Builder
       </h1>
-      <Button type="primary">Primary Button</Button>
-      <Button type="default">Default Button</Button>
-      <Input placeholder='Basic input...' />
+      <UserButton />
+      <h1>Name: {name}</h1>
+      <h1>Email: {email}</h1>
+      <h1>Clerk User ID: {clerkUserId}</h1>
     </div>
   );
 }
